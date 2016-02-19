4ed87d84bdcf2122b574281ab7f7a4c7297d4578
Nelson Cheung <nelsoncheung88@hotmail.com>
2016-02-19 10:48:46 +0800

    My first GIT submit for testing

---
+++ Win32Project2/Source.cpp
@@ -0,0 +1,147 @@
+#include <windows.h>
+#include "resource.h"
+
+const wchar_t g_szClassName[] = L"myWindowClass";
+
+//test here
+
+BOOL CALLBACK DialogProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
+{
+	switch (Message)
+	{
+	case WM_INITDIALOG:
+
+		return TRUE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			EndDialog(hwnd, IDOK);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, IDCANCEL);
+			break;
+		}
+		break;
+	default:
+		return FALSE;
+	}
+	return TRUE;
+}
+
+
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_LBUTTONDOWN:
+	{
+		wchar_t szFileName[MAX_PATH];
+		HINSTANCE hInstance = GetModuleHandle(NULL);
+
+		GetModuleFileName(hInstance, szFileName, MAX_PATH);
+		MessageBox(hwnd, szFileName, L"This program is:", MB_OK | MB_ICONINFORMATION);
+	/*	PostMessage(hwnd, WM_DESTROY, 0, 0);*/
+	}
+	break;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_OPEN:
+			{
+				int ret = DialogBox(GetModuleHandle(NULL),
+					MAKEINTRESOURCE(IDD_DIALOG1), hwnd, DialogProc);
+				if (ret == IDOK) {
+					MessageBoxA(hwnd, "Dialog exited with IDOK.", "Notice",
+						MB_OK | MB_ICONINFORMATION);
+				}
+				else if (ret == IDCANCEL) {
+					MessageBoxA(hwnd, "Dialog exited with IDCANCEL.", "Notice",
+						MB_OK | MB_ICONINFORMATION);
+				}
+				else if (ret == -1) {
+					MessageBoxA(hwnd, "Dialog failed!", "Error",
+						MB_OK | MB_ICONINFORMATION);
+				}
+			}
+			break;
+		case ID_FILE_EXIT:
+			PostMessage(hwnd, WM_CLOSE, 0, 0);
+			break;
+		}
+		break;
+
+	case WM_CLOSE:
+		/*MessageBoxA(NULL, "WM_CLOSE", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);*/
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		/*MessageBoxA(NULL, "WM_DESTROY", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);*/
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
+
+	//Step 1: Registering the Window Class
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = 0;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBoxA(NULL, "Window Registration Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	// Step 2: Creating the Window
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		g_szClassName,
+		L"The title of my window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
+		NULL, NULL, hInstance, NULL);
+
+	if (hwnd == NULL)
+	{
+		MessageBoxA(NULL, "Window Creation Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	// Step 3: The Message Loop
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
+}
